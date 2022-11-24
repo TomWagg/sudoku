909e30db4043f1e9f2989febe4239d2b08d47d05
Tom Wagg <tomjwagg@gmail.com>
2022-11-24 18:17:33 -0500

    set up python framework

---
+++ python/solve.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+
+def solve_sudoku(grid):
+    # solve a sudoku puzzle
+    raise NotImplementedError
---
+++ python/utils.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def grid_is_valid(grid):
+    # check whether a sudoku grid is valid
+    raise NotImplementedError
+
+
+def generate_grid():
+    # create a valid sudoku grid
+    raise NotImplementedError
