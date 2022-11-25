825906ff3040e0e3202dd498997719f35cdf6ef5
Tom Wagg <tomjwagg@gmail.com>
2022-11-24 19:59:00 -0500

    add comments to validation

--- python/validate.py
+++ python/validate.py
@@ -2,7 +2,23 @@ import numpy as np
 
 
 def grid_is_valid(grid, verbose=False):
-    # check whether a sudoku grid is valid
+    """Check whether a sudoku grid is valid
+
+    Check that each row has no repeated numbers, each column has no repeated numbers and that each 3x3 box
+    has no repeated numbers.
+
+    Parameters
+    ----------
+    grid : `list` of `lists`
+        _description_
+    verbose : `bool`, optional
+        Whether to print out why a grid is invalid, by default False
+
+    Returns
+    -------
+    valid : `bool`
+        Whether the grid is valid
+    """
     if not has_valid_rows(grid):
         if verbose:
             print("A row has a repeated entry")
@@ -19,6 +35,18 @@ def grid_is_valid(grid, verbose=False):
 
 
 def has_valid_boxes(grid):
+    """Check whether a sudoku grid has valid 3x3 boxes
+
+    Parameters
+    ----------
+    grid : _type_
+        _description_
+
+    Returns
+    -------
+    valid : `bool`
+        Whether the grid has valid boxes
+    """
     for rs in range(3):
         for cs in range(3):
             box = grid[rs * 3:(rs + 1) * 3, cs * 3:(cs + 1) * 3]
@@ -30,10 +58,27 @@ def has_valid_boxes(grid):
 
 
 def has_valid_rows(grid):
+    """Check whether a sudoku grid has valid rows
+
+    NOTE: You can pass the transpose of the grid to check the columns
+
+    Parameters
+    ----------
+    grid : _type_
+        _description_
+
+    Returns
+    -------
+    valid : `bool`
+        Whether the grid has valid rows
+    """
     for row in grid:
-        # print(row)
+        # check if the row has any filled values (0 = empty)
         if any(row) > 0:
+            # check how many unique entries there are for each value
             _, counts = np.unique(row[row > 0], return_counts=True)
+
+            # if any are repeated then grid is invalid
             if any(counts > 1):
                 return False
     return True
