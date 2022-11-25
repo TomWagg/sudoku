0dfa1249114826df4fe715f015e848193d013925
Tom Wagg <tomjwagg@gmail.com>
2022-11-24 19:00:31 -0500

    add row and column validation

--- python/utils.py
+++ python/utils.py
@@ -1,26 +1,47 @@
 import numpy as np
 
 
-def grid_is_valid(grid):
+def grid_is_valid(grid, verbose=False):
     # check whether a sudoku grid is valid
-    raise NotImplementedError
+    if not has_valid_rows(grid):
+        print("A row has a repeated entry")
+        return False
+    if not has_valid_rows(grid.T):
+        print("A column has a repeated entry")
+        return False
+    if not has_valid_boxes(grid):
+        print("A box has a repeated entry")
+        return False
+
+
+def has_valid_boxes(grid):
+    return True
+
+
+def has_valid_rows(grid):
+    for row in grid:
+        # print(row)
+        if any(row) > 0:
+            uniques, counts = np.unique(row[row > 0], return_counts=True)
+            if any(counts > 1):
+                return False
+    return True
 
 
 def generate_grid(n_filled):
-    # create a valid sudoku grid
+    # create a blank sudoku grid
+    grid = np.zeros(shape=(9, 9)).astype(int)
 
-    grid = np.array([[0 for i in range(9)] for j in range(9)])
-
+    # pick some random indices and convert them into rows and columns
     indices = np.random.choice(81, n_filled, replace=False)
-
-    print(indices)
     rows = indices // 9
     columns = indices % 9
 
+    # choose some random entries for those rows and columns
     entries = np.random.randint(1, 10, n_filled)
 
+    # populate grid with random entries
     grid[rows, columns] = entries
-
     return grid
 
 
@@ -43,4 +64,6 @@ def print_grid(grid):
             print()
 
 
-print_grid(generate_grid(20))
+grid = generate_grid(20)
+print_grid(grid)
+print(grid_is_valid(grid))
