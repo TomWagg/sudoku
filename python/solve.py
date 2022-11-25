adb70a3290358540fd61cfd12b70c27eebcfaf15
Tom Wagg <tomjwagg@gmail.com>
2022-11-25 15:51:03 +0000

    refactor solving into algorithms, add comments

--- python/solve.py
+++ python/solve.py
@@ -1,9 +1,67 @@
 import numpy as np
-from utils import ind2row_col, empty_k_grid_spots, print_grid
+from utils import ind2row_col, empty_k_grid_spots, print_grid, parse_grid
 from validate import is_valid_move
 
 
-def solve_sudoku(grid, shuffle=False, stepbystep=False):
+def solve_sudoku(grid, algorithm="retracing", shuffle=False, stepbystep=False):
+    """Solve a sudoku puzzle!
+
+    Parameters
+    ----------
+    grid : `np.ndarray` or `str`
+        Sudoku grid or string representation of grid
+    algorithm : `str`, optional
+        Which algorithm to use, by default "retracing"
+    shuffle : `bool`, optional
+        Whether to randomly choose which number to try instead of in order, by default False
+    stepbystep : `bool`, optional
+        Whether to print out the step-by-step solution, by default False
+
+    Returns
+    -------
+    solved_grid : `np.ndarray`
+        The fully filled in grid (if a solution is possible)
+    can_be_solved : `bool`
+        Whether the puzzle can be solved
+
+    Raises
+    ------
+    ValueError
+        If an invalid algorithm name is passed
+    """
+    # parse out a grid from a string if necessary
+    if isinstance(grid, str):
+        grid = parse_grid(grid)
+
+    # solve the sudoku using the requested algorithm
+    if algorithm == "retracing":
+        return _solve_sudoku_retracing(grid, shuffle=shuffle, stepbystep=stepbystep)
+    else:
+        raise ValueError(f"Invalid algorithm name: `{algorithm}`")
+
+
+def _solve_sudoku_retracing(grid, shuffle=False, stepbystep=False):
+    """Solve a sudoku puzzle using the "retracing your steps" algorithm.
+
+    For each empty entry, check if a number can be put there and the grid remain valid. If yes recurse, if no
+    try the next number. If no numbers left then no solution. If no empty spots left then solution is complete
+
+    Parameters
+    ----------
+    grid : `np.ndarray` or `str`
+        Sudoku grid or string representation of grid
+    shuffle : `bool`, optional
+        Whether to randomly choose which number to try instead of in order, by default False
+    stepbystep : `bool`, optional
+        Whether to print out the step-by-step solution, by default False
+
+    Returns
+    -------
+    solved_grid : `np.ndarray`
+        The fully filled in grid (if a solution is possible)
+    can_be_solved : `bool`
+        Whether the puzzle can be solved
+    """
     if stepbystep:
         print_grid(grid)
     # flattened version of the 9x9 grid
@@ -43,11 +101,28 @@ def solve_sudoku(grid, shuffle=False, stepbystep=False):
 
 
 def generate_solvable_grid(n_filled):
-    # generate a fully solved grid, then erase some values!
+    """Generate a fully solvable sudoku grid with `n_filled` non-empty squares
+
+    Parameters
+    ----------
+    n_filled : `int`
+        Number of squares to fill in
+
+    Returns
+    -------
+    grid : `np.ndarray`
+        The generated grid
+    """
+    # start with an empty grid
     grid = np.zeros((9, 9)).astype(int)
+
+    # fill in the diagonals independently since they can't affect one another
     grid = _fill_diagonals(grid)
+
+    # iteratively solve the puzzle
     grid, _ = solve_sudoku(grid, shuffle=True)
 
+    # erase the required number of squares
     n_remove = max(81 - n_filled, 0)
     if n_remove > 0:
         grid = empty_k_grid_spots(grid, n_remove)
